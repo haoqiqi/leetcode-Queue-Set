a4958e9a92989a530b4dd6e8d313e44eff2ab7a7
haoqiqi <15133551208@163.com>
2020-11-20 15:59:33 +0800

    队列和栈

---
+++ src/com/haoqiqi/demo/User.java
@@ -0,0 +1,14 @@
+package com.haoqiqi.demo;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
